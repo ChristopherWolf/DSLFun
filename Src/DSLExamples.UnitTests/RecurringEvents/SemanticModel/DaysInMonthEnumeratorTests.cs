493d261489f916d00fc721d02fc3f4a80cba4d92
ChristopherWolf <christopherw@iqmetrix.com>
2013-12-02 14:06:37 -0600

    Added DaysInMonthEnumerator

---
+++ Src/DSLExamples.UnitTests/RecurringEvents/SemanticModel/DaysInMonthEnumeratorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.UnitTests.TestingHelpers;
+using DSLExamples.RecurringEvents.SemanticModel;
+using FluentAssertions;
+using Ploeh.AutoFixture;
+using Xunit.Extensions;
+
+// ReSharper disable CheckNamespace
+namespace DSLExamples.UnitTests.RecurringEvents.SemanticModel.MonthTests.DaysInMonthEnumeratorTests
+// ReSharper restore CheckNamespace
+{
+	public class WhenVerifyingArchitecturalConstraints
+	{
+		[Theory, AutoFakeItEasyData]
+		public void ItShouldBe(IFixture fixture, DateTime date)
+		{
+			// Arrange
+			var sut = new DaysInMonthEnumerator((uint)date.Month, (uint)date.Year);
+
+			// Act and Assert
+			sut.Should().BeAssignableTo<IEnumerable<DateTime>>();
+		}
+	}
+
+	public class WhenEnumeratingTheSut
+	{
+		[Theory, AutoFakeItEasyData]
+		public void ItShouldReturnExpectedValues(DateTime testDate)
+		{
+			// Arrange
+			var firstDayOfMonth = new DateTime(testDate.Year, testDate.Month, 1);
+			var firstDayOfNextMonth = firstDayOfMonth.AddMonths(1);
+
+			var expected = GetDays(firstDayOfMonth, firstDayOfNextMonth).ToArray();
+
+			var sut = new DaysInMonthEnumerator((uint)testDate.Month, (uint)testDate.Year);
+
+			// Act
+			var result = sut.ToArray();
+
+			// Assert
+			result.Should().Equal(expected);
+		}
+
+		IEnumerable<DateTime> GetDays(DateTime start, DateTime end)
+		{
+			// Remove time info from start date (we only care about day). 
+			var current = new DateTime(start.Year, start.Month, start.Day);
+
+			while (current < end)
+			{
+				yield return current;
+				current = current.AddDays(1);
+			}
+		}
+	}
+}
---
+++ Src/DSLExamples/RecurringEvents/SemanticModel/DaysInMonthEnumerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DSLExamples.RecurringEvents.SemanticModel
+{
+	/// <summary>
+	/// An IEnumerable 'DateTime' that begins on the first day of the passed in month and year
+	/// and returns a sequence containing every day in that month until the next month.
+	/// </summary>
+	public class DaysInMonthEnumerator : IEnumerable<DateTime>
+	{
+		readonly DateTime _start;
+		readonly DateTime _end;
+
+		public DaysInMonthEnumerator(uint month, uint year)
+			: this((int)month, (int)year)
+		{
+		}
+
+		DaysInMonthEnumerator(int month, int year)
+		{
+			_start = new DateTime(year, month, 1);
+			_end = _start.AddMonths(1);
+		}
+
+		public IEnumerator<DateTime> GetEnumerator()
+		{
+			var current = _start;
+
+			while (current < _end)
+			{
+				yield return current;
+				current = current.AddDays(1);
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
