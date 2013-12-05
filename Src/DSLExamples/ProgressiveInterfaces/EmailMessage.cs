8c1e818a8333b1698fbd6cf88711e505e7a63fd3
ChristopherWolf <christopherw@iqmetrix.com>
2013-12-05 15:56:04 -0600

    Added fluent builder using progressive interfaces

---
+++ Src/DSLExamples/ProgressiveInterfaces/EmailMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSLExamples.ProgressiveInterfaces
+{
+	public class EmailMessage
+	{
+		readonly IEnumerable<string> _toList;
+		readonly IEnumerable<string> _ccList;
+		readonly string _subject;
+		readonly string _body;
+
+		public EmailMessage(IEnumerable<string> toList, IEnumerable<string> ccList, string subject, string body)
+		{
+			if (toList == null) throw new ArgumentNullException("toList");
+			if (subject == null) throw new ArgumentNullException("subject");
+			if (body == null) throw new ArgumentNullException("body");
+			if (ccList == null) throw new ArgumentNullException("ccList");
+
+			_toList = toList;
+			_subject = subject;
+			_body = body;
+			_ccList = ccList;
+		}
+
+		public IEnumerable<string> ToList { get { return _toList; } }
+
+		public IEnumerable<string> CcList { get { return _ccList; } }
+
+		public string Subject { get { return _subject; } }
+
+		public string Body { get { return _body; } }
+	}
+}
---
+++ Src/DSLExamples/ProgressiveInterfaces/EmailMessageBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSLExamples.ProgressiveInterfaces
+{
+	public interface IMessageBuilderPostBuild
+	{
+		IMessageBuilderPostTo To(string to);
+	}
+
+	public interface IMessageBuilderPostTo : IMessageBuilderPostBuild
+	{
+		IMessageBuilderPostCc Cc(string cc);
+		IMessageBuilderPostSubject Subject(string subject);
+	}
+
+	public interface IMessageBuilderPostCc
+	{
+		IMessageBuilderPostCc Cc(string cc);
+		IMessageBuilderPostSubject Subject(string subject);
+	}
+
+	public interface IMessageBuilderPostSubject
+	{
+		EmailMessage Body(string body);
+	}
+
+	public class EmailMessageBuilder : IMessageBuilderPostBuild, IMessageBuilderPostTo, IMessageBuilderPostCc, IMessageBuilderPostSubject
+	{
+		string _subject;
+
+		readonly IList<string> _toList;
+		readonly IList<string> _ccList;
+
+		EmailMessageBuilder()
+		{
+			_toList = new List<string>();
+			_ccList = new List<string>();
+		}
+
+		public static IMessageBuilderPostBuild Build()
+		{
+			return new EmailMessageBuilder();
+		}
+
+		public IMessageBuilderPostTo To(string to)
+		{
+			_toList.Add(to);
+
+			return this;
+		}
+
+		public IMessageBuilderPostCc Cc(string cc)
+		{
+			_ccList.Add(cc);
+
+			return this;
+		}
+
+		public IMessageBuilderPostSubject Subject(string subject)
+		{
+			_subject = subject;
+
+			return this;
+		}
+
+		public EmailMessage Body(string body)
+		{
+			return new EmailMessage(_toList, _ccList, _subject, body);
+		}
+	}
+
+
+	public class BuilderConsumer
+	{
+		public BuilderConsumer()
+		{
+			var email = EmailMessageBuilder.Build()
+			                               .To("").To("")
+			                               .Cc("").Cc("")
+			                               .Subject("")
+			                               .Body("");
+
+			Console.WriteLine(email);
+		}
+	}
+}
